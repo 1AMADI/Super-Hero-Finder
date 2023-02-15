58897716a56ce7a9b7e61c84f2c4769b5fa871aa
Aditya Vijay Singh <112424260+1AMADI@users.noreply.github.com>
2023-02-16 00:37:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,13 +8,8 @@ const getHeroImage=(id)=>{
     fetch(`${baseUrl}/${id}`)
     .then(response=>response.json())
     .then(json=>{
-        // This fetches the name of the hero from the json file and binds it to the variable heroName.
+        showHeroStats(json)
 
-        const heroName=`<h2>${json.name}</h2>`
-        const stats=showHeroStats(json)
-        //This changes the inner html of the image div for inserting the picture as well as adds a h2 which has the name of the character.
-
-        image.innerHTML=`${heroName}<img id="superHeroImage" src="${json.image.url}" height=300 width=300 <br> ${stats}`
 })
 }
 
@@ -23,13 +18,11 @@ const searchedHero=(name)=>{
     fetch(`${baseUrl}/search/${name}`)
     .then(response=>response.json())
     .then(json=>{
-       
         const hero=json.results[0]
-        const stats=showHeroStats(hero)
-        const heroName=`<h2>${hero.name}</h2>`
-        image.innerHTML=`${heroName}<img id="superHeroImage" src="${hero.image.url}" height=300 width=300/ <br> ${stats}`
-})
+        showHeroStats(hero);
+    })
 }
+// this is an object consisting of emojis to bind with the powerstats
 const statToEmoji={
     intelligence:"🧠",
     strength:"💪",
@@ -38,12 +31,18 @@ const statToEmoji={
     power:"📊",
     combat:"⚔️"
 }
+// this is a function that handles all the work of getting the name , image and all the power stats concerned with the required hero
 const showHeroStats=(character)=>{
+    // this line gets the name from the character.name and binds it to the variable heroName
+    const heroName=`<h2>${character.name}</h2>`
+    // this line gets the image url of the hero and binds the url to the img variable
+    const img=`<img id="superHeroImage" src="${character.image.url}" height=300 width=300 />`
+    // Object.keys is used to convert any object keys into an array and later we iterate through it using the .map function and joins all the values of the stats to it and later joins all the array using the .join() method.
+
     const stats=Object.keys(character.powerstats).map(stat=>{
-        return `<p>${statToEmoji[stat]} ${stat.toUpperCase()} :${character.powerstats[stat]}</p>`})
-    return stats.join("")
-
-
+        return `<p>${statToEmoji[stat]} ${stat.toUpperCase()} :${character.powerstats[stat]}</p>`}).join('')
+        //This line updates the innerHTML of the image div to the following which includes the variables name , img, stats.
+        image.innerHTML=`${heroName} ${img} <br> ${stats}>`
 }
 button.onclick=()=>{
     // this random num randomly selects  a number out of 730 available hero ids and calls the function with that id number as input
